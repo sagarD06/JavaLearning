d1c384a2b6242f86e0d5de13042a25a6b26c630a
Sagar D <sagardg33@gmail.com>
2026-07-08 08:47:30 +0530

    Implimnets Interface and Autowire to fetch the object refs automatically byName or byType

--- SpringProject/src/main/java/org/example/Alien.java
+++ SpringProject/src/main/java/org/example/Alien.java
@@ -2,15 +2,15 @@ package org.example;
 
 public class Alien {
     private int age;
-    private Laptop lap;
+    private Computer com;
 
     public Alien () {
         System.out.println("Object Created!");
     }
 
-    public Alien(int age, Laptop lap) {
+    public Alien(int age, Computer com) {
         this.age = age;
-        this.lap = lap;
+        this.com = com;
     }
 
     public int getAge() {
@@ -21,16 +21,16 @@ public class Alien {
         this.age = age;
     }
 
-    public Laptop getLap() {
-        return lap;
+    public Computer getcom() {
+        return com;
     }
 
-    public void setLap(Laptop lap) {
-        this.lap = lap;
+    public void setcom(Computer com) {
+        this.com = com;
     }
 
     public void code (){
         System.out.println("Coding...");
-        lap.compile();
+        com.compile();
     }
 }
--- SpringProject/src/main/java/org/example/Laptop.java
+++ SpringProject/src/main/java/org/example/Laptop.java
@@ -1,7 +1,8 @@
 package org.example;
 
-public class Laptop {
+public class Laptop implements Computer {
 
+    @Override
     public void compile(){
         System.out.println("Laptop compiling...");
     }

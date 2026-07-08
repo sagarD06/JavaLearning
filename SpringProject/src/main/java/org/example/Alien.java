5c1336bf30e7eb8c6b8b991a3c55cc7c49eca4db
Sagar D <sagardg33@gmail.com>
2026-07-08 08:02:21 +0530

    Constructor injection

--- SpringProject/src/main/java/org/example/Alien.java
+++ SpringProject/src/main/java/org/example/Alien.java
@@ -2,9 +2,15 @@ package org.example;
 
 public class Alien {
     private int age;
+    private Laptop lap;
 
     public Alien () {
+        System.out.println("Object Created!");
+    }
 
+    public Alien(int age, Laptop lap) {
+        this.age = age;
+        this.lap = lap;
     }
 
     public int getAge() {
@@ -15,7 +21,16 @@ public class Alien {
         this.age = age;
     }
 
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code (){
         System.out.println("Coding...");
+        lap.compile();
     }
 }

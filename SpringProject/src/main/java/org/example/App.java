3bd5d607f80e21e8374ac652faaf83648e225642
Sagar D <sagardg33@gmail.com>
2026-07-10 08:23:33 +0530

    Replacing XML config with JAVA based configurations

--- SpringProject/src/main/java/org/example/App.java
+++ SpringProject/src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,9 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj = context.getBean("alien", Alien.class);
-        obj.code();
-        System.out.println(obj.getAge());
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dk = context.getBean(Desktop.class);
+        dk.compile();
+
+        //below is the XML way of configuration
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        Alien obj = context.getBean("alien", Alien.class);
+//        obj.code();
+//        System.out.println(obj.getAge());
     }
 }

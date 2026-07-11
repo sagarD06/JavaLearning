6efd221879ecf503f2869db7ed426e008b1f0594
Sagar D <sagardg33@gmail.com>
2026-07-11 06:58:58 +0530

    Java Based Primary and qualifier refs

--- SpringProject/src/main/java/org/example/config/AppConfig.java
+++ SpringProject/src/main/java/org/example/config/AppConfig.java
@@ -3,15 +3,18 @@ package org.example.config;
 import org.example.Alien;
 import org.example.Computer;
 import org.example.Desktop;
+import org.example.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public Alien alien(Computer com) {
+    public Alien alien(Computer com) { //@Qualifier("desktop") 
         Alien obj = new Alien();
         obj.setAge(25);
         obj.setcom(com);
@@ -21,5 +24,11 @@ public class AppConfig {
     @Bean
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
